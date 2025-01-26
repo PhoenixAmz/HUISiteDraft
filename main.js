70e4fe919b68e06bf6415f4ad7ae9cac1770382d
PhoenixAmz <64906129+PhoenixAmz@users.noreply.github.com>
2025-01-26 18:41:56 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+/* $(document).ready(function(){
+    
+window.prompt('what is your name')
+
+
+
+// will need create something that can add new locations?? though that may be a back end fucntion.
+
+
+
+
+// The album locations 'read more' buttons link ot a pdf on the location.
+
+}) */
+
