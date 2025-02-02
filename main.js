a8951308a90b4f23dd64d3cb61213d823b3e19aa
PhoenixAmz <64906129+PhoenixAmz@users.noreply.github.com>
2025-02-02 20:08:33 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ window.prompt('what is your name')
 
 // will need create something that can add new locations?? though that may be a back end fucntion.
 
-
+// create the onclick event listeners for all buttons
 
 
 // The album locations 'read more' buttons link ot a pdf on the location.
